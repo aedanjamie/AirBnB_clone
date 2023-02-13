857fd31e0f944935ed9db3dc3ed635fd59c5238f
aedanjamie <aedanjamie@gmail.com>
2023-02-13 04:06:21 -0800

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -30,8 +30,8 @@ class BaseModel:
                     self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
             storage.new(self)
 
     def __str__(self):
